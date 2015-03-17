0ef226d523dccd42c856a8c418ca0469fa926f45
Benjamin <Benjamin@BBN>
2015-03-17 16:47:47 +0100

    Person, Employee, Student (model) EmployeeHandler, StudentHandler (handlers) temp DBConnection fil som placeholder.

---
+++ src/view/CustomScene.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class CustomScene extends Scene{
+
+    public CustomScene(Parent root) {
+        super(root);
+        
+        
+    }
+    
+}
