7b63017a3fc70c3752771733d64ea4a9c4f3f14d
Jonas <Jonas@10.200.124.142>
2015-04-28 11:09:31 +0200

    Merge origin/master
    Conflicts:
    	test/handlers/RoomHandlerTest.java

---
+++ src/view/schema/ShiftPeriodConstants.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.schema;
+
+import org.joda.time.Hours;
+import org.joda.time.Minutes;
+
+/**
+ *
+ * @author Jonas
+ */
+public class ShiftPeriodConstants {
+    
+    public static final Hours DAY_SHIFT_HOURS_START = Hours.hours(7);
+    public static final Minutes DAY_SHIFT_MINUTES_START = Minutes.minutes(30);
+    public static final Hours EVENING_SHIFT_HOURS_START = Hours.hours(15);
+    public static final Minutes EVENING_SHIFT_MINUTES_START = Minutes.minutes(15);
+    public static final Hours NIGHT_SHIFT_HOURS_START = Hours.hours(23);
+    public static final Minutes NIGHT_SHIFT_MINUTES_START = Minutes.minutes(30);
+    
+    
+}
